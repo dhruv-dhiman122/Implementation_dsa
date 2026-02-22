3c169f8f5a7f668f640ba1eed2abdcebc9240749
Dhruv Dhiman <dhruvdhiman770@gmail.com>
2026-02-23 00:22:23 +0530

    Made the file for implementation of single linked list

---
+++ c-lang/single_linkedlist.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//========================== Implementation of single linked list ==============================
+
+struct Node {
+  int data;
+  struct Node* next;
+};
+
+
+
+
+int main() {
+
+  return 0;
+}
